b8a4476edbdce629a45879d9e36c9804b4acf62e
Matt Davidson <matthewdavidson12@gmail.com>
2015-12-05 10:22:13 +0000

    solution added for day 2 part 2

--- tests/test_02.py
+++ tests/test_02.py
@@ -1,5 +1,9 @@
 import pytest
-from advent.problem_02 import (calculate_area, format_lines, total_area)
+from advent.problem_02 import (calculate_area,
+                               calculate_wribbon,
+                               format_lines,
+                               total_area,
+                               total_wribbon)
 
 
 def test_format_lines():
@@ -19,3 +23,16 @@ def test_total_area():
     assert total_area('2x3x4\n') == 58
     assert total_area('2x3x4\n0x0x0\n') == 58
     assert total_area('2x3x4\n1x1x10\n') == 58+43
+
+
+def test_calculate_wribbon():
+    assert calculate_wribbon([0, 0, 0]) == 0
+    assert calculate_wribbon([2, 3, 4]) == 34
+    assert calculate_wribbon([1, 1, 10]) == 14
+
+
+def test_total_wribbon():
+    assert total_wribbon('0x0x0\n') == 0
+    assert total_wribbon('2x3x4\n') == 34
+    assert total_wribbon('2x3x4\n0x0x0\n') == 34
+    assert total_wribbon('2x3x4\n1x1x10\n') == 34+14
