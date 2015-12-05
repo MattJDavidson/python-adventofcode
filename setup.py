967a15744e3e603d1030e6bca29d523851ba8e04
Matt Davidson <matthewdavidson12@gmail.com>
2015-12-05 11:28:18 +0000

    general dir cleanup; setup.py test works

--- setup.py
+++ setup.py
@@ -3,17 +3,25 @@
 from __future__ import absolute_import, print_function
 
 import io
-import os
 import re
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import relpath
-from os.path import splitext
+from os.path import (dirname, join)
 
-from setuptools import find_packages
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+import advent
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        pytest.main(self.test_args)
 
 
 def read(*names, **kwargs):
@@ -25,21 +33,20 @@ def read(*names, **kwargs):
 
 setup(
     name='advent',
-    version='0.1.0',
+    version=advent.__version__,
+    url='https://github.com/mattjdavidson/python-adventofcode',
+    author='Matt Davidson',
+    author_email='@matt_davidson_',
     license='BSD',
     description='TDD practice and packaging plaything wrapped around adventofcode.com solutions.',
     long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M|re.S).sub('', read('README.rst')),
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub(
+            '', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
     ),
-    author='Matt J Davidson',
-    author_email='@matt_davidson_',
-    url='https://github.com/mattjdavidson/python-adventofcode',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    packages=['advent'],
     include_package_data=True,
-    zip_safe=False,
+    platforms='any',
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
@@ -60,22 +67,6 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    install_requires=[
-        'click==6.2',
-        'wheel==0.24.0',
-    ],
-    tests_requires=[
-        'py==1.4.31',
-        'pytest==2.8.3',
-    ],
-    extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
-    },
-    entry_points={
-        'console_scripts': [
-            'advent = advent.cli:main',
-        ]
-    },
+    install_requires=[],
+    cmdclass={'test': PyTest},
 )
